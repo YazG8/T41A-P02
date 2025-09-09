c566ed1b37a3ce454134deadbc3201d7cd0d78f9
Yaz Guerrero <yazmin.guerreroguev@gmail.com>
2025-09-08 19:22:10 -0600

    feature/182483

--- tests/test_database.py
+++ tests/test_database.py
@@ -15,25 +15,25 @@ def db_connection():
 
 def test_alumnos_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM alumnos;")
+        cur.execute("SELECT COUNT(*) FROM alumno;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_maestros_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM maestros;")
+        cur.execute("SELECT COUNT(*) FROM profesor;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_grupos_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM grupos;")
+        cur.execute("SELECT COUNT(*) FROM curso;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_inscripciones_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM inscripciones;")
+        cur.execute("SELECT COUNT(*) FROM inscripcion;")
         count = cur.fetchone()[0]
         assert count == 10  
 
@@ -47,11 +47,11 @@ def test_structure(db_connection):
     sql = ''' 
             SELECT tablename FROM pg_tables 
             WHERE tablename 
-            IN ('alumnos', 'maestros', 'grupos',
-            'inscripciones', 'asistencia');
+            IN ('alumno', 'profesor', 'curso',
+            'inscripcion', 'asistencia');
           '''
-    expected_tables = {'alumnos', 'maestros',
-                       'grupos', 'inscripciones',
+    expected_tables = {'alumno', 'profesor',
+                       'curso', 'inscripcion',
                        'asistencia'}
     with db_connection.cursor() as cur:
         cur.execute(sql)
